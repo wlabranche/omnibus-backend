e70c3cc4d73142b0eeb135fe28bd82206184dcf8
wcjohnson11 <williamcouperjohnson@gmail.com>
2014-08-04 20:11:58 -0700

    is it deployed yet?

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,14 @@ var gulp = require('gulp');
 var istanbul = require('gulp-istanbul');
 var mocha = require('gulp-mocha');
 var lcov = require('mocha-lcov-reporter');
+// var bower = require('bower');
 
+// gulp.task('install',['test'] function(){
+//   return bower.commands.install()
+//   .on('log', function(data) {
+//     gutil.log('bower', gutil.colors.cyan(data.id), data.message);
+//   });
+// });
 
 gulp.task('test', function(cb){
   gulp.src([
