c9d1d4d85e38bc3679924e7177ec954bfd41bd40
Nick Bottomley <nhbottomley@gmail.com>
2014-08-06 10:29:08 -0700

    send responses from NYT Congress API

--- routes/bills/index.js
+++ routes/bills/index.js
@@ -8,10 +8,13 @@ var Congress = require( 'nyt-congress-node' );
 var client = new Congress( process.env.NYT_CONGRESS_KEY );
 
 router.get( '/:id', function ( req, res ) {
+  var id = req.params.id.split( '-' );
   client.billDetails({
-    billId: req.params.id
+    billId: id[1],
+    congressNumber: id[0]
+  }).then( function( data ) {
+    res.json( data );
   });
-  res.send( req.params.id );
 });
 
 module.exports = router;
