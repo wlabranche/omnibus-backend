0496c3e11afa854c4ea4ee2c37d2a61ea6d1c3af
Nick Bottomley <nhbottomley@gmail.com>
2014-08-11 11:15:58 -0700

    Fix /bills/:id/versions

--- routes/bills/search/test.js
+++ routes/bills/search/test.js
@@ -1,5 +1,5 @@
 require( '../single-endpoint-test' )({
   nockRoot: 'https://congress.api.sunlightfoundation.com',
-  nockPath: '/bills/search/?query=test&apikey=test&query=test&apikey=test',
+  nockPath: '/bills/search/?query=test&apikey=test&query=test',
   appPath: '/bills/search?q=test'
 });
