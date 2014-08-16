40506a7df78fd0180d2495680d3cfba8cfa43fac
Nick Bottomley <nhbottomley@gmail.com>
2014-08-16 15:22:25 -0700

    Messing with tests

--- routes/bills/votes/test.js
+++
@@ -1,9 +0,0 @@
-// need to come up with a better way of testing when we're hitting multiple end points.
-
-// require( '../single-endpoint-test' )({
-//   nockRoot: 'https://congress.api.sunlightfoundation.com',
-//   nockPath: '/votes/?bill_id=hr2397-113&apikey=test&bill_id=hr2397-113',
-//   appPath: '/bills/:id/votes'
-// });
-
-
--- routes/congress/enacted/index.js
+++ routes/congress/enacted/index.js
@@ -1,15 +1,5 @@
 'use strict';
 
-// var Promise = require( 'bluebird' );
-// var TimesApi = require( 'nyt-congress-node' );
-// var sunlightApi = require( 'sunlight-congress-api' );
-
-// var SUNLIGHT = process.env.SUNLIGHT_CONGRESS_KEY || 'test';
-// sunlightApi.init( SUNLIGHT );
-
-// var NYT = process.env.NYT_CONGRESS_KEY || 'test';
-// var timesApi = new TimesApi( NYT );
-
 var sunlightAll = require( '../../sunlight-all' );
 
 module.exports = function ( req ) {
